f02f93b68bed704082ae14cbbf3fbda6921e4651
Cesar Flores Palacios <90871173+CesarFlores55@users.noreply.github.com>
2024-08-18 23:34:29 -0500

    queue music commands

---
+++ src/old/main.rs
@@ -0,0 +1,38 @@
+// use poise::serenity_prelude as serenity;
+// use dotenv::dotenv;
+// use std::{env, sync::{Arc, Mutex}};
+// use tokio::signal;
+// mod slash_commands;
+// use unamed_bot::types::Data;
+// use crate::slash_commands::age::age;
+// #[tokio::main]
+// async fn main() {
+//     dotenv().ok();
+//     let token = env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+//     let intents = serenity::GatewayIntents::non_privileged();
+//     let framework = poise::Framework::builder()
+//         .options(poise::FrameworkOptions {
+//             slash_commands: vec![age()],
+//             ..Default::default()
+//         })
+//         .setup(|ctx, _ready, framework| {
+//             Box::pin(async move {
+//                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+//                 Ok(Data {})
+//             })
+//         })
+//         .build();
+//     let mut client = serenity::ClientBuilder::new(token, intents)
+//         .framework(framework)
+//         .await
+//         .expect("Error creating client");
+//     let shard_manager = Arc::new(Mutex::new(client.shard_manager.clone()));
+//     tokio::spawn(async move {
+//         if let Err(err) = client.start().await {
+//             eprintln!("Client error: {:?}", err);
+//         }
+//     });
+//     signal::ctrl_c().await.expect("Failed to listen for Ctrl+C");
+//     println!("Received Ctrl+C, shutting down...");
+//     shard_manager.lock().unwrap().shutdown_all().await;
+// }
